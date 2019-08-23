984f414df22238cd53b7200cff55c51e21860c2f
devHawk42 <devhawk42@gmail.com>
2019-08-23 13:04:29 -0300

    add popup on hover

--- src/App.js
+++ src/App.js
@@ -10,6 +10,8 @@ class App extends Component {
             container: this.mapContainer,
             style: 'mapbox://styles/mapbox/streets-v11'
         });
+
+        
         this.addSearch();
 
         this.drawPoints();
@@ -26,7 +28,7 @@ class App extends Component {
         let self = this;
         
         let {features} = await makeRequest.getAll();
-
+        
         this.map.on("load", function() {
             self.map.addSource("national-park", {
                 "type": "geojson",
@@ -47,6 +49,42 @@ class App extends Component {
                 "filter": ["==", "$type", "Point"],
             });
         })
+
+        // Create a popup, but don't add it to the map yet.
+        var popup = new mapboxgl.Popup({
+            closeButton: false,
+            closeOnClick: false
+        });
+
+        this.map.on('mouseenter', 'park-volcanoes', function(e) {
+            // Change the cursor style as a UI indicator.
+            self.map.getCanvas().style.cursor = 'pointer';
+            console.log(e)
+            var coordinates = e.features[0].geometry.coordinates.slice();
+            var description = self.getHTMLDescription(e.features[0].properties);
+            
+            // Ensure that if the map is zoomed out such that multiple
+            // copies of the feature are visible, the popup appears
+            // over the copy being pointed to.
+            while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+                coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+            }
+            
+            // Populate the popup and set its coordinates
+            // based on the feature found.
+            popup.setLngLat(coordinates)
+            .setHTML(description)
+            .addTo(self.map);
+        });
+            
+        this.map.on('mouseleave', 'park-volcanoes', function() {
+            self.map.getCanvas().style.cursor = '';
+            popup.remove();
+        });
+    }
+
+    getHTMLDescription(properties){
+        return "<strong>Make it Mount Pleasant</strong><p>Make it Mount Pleasant is a handmade and vintage market and afternoon of live entertainment and kids activities. 12:00-6:00 p.m.</p>";
     }
 
     componentWillUnmount() {
